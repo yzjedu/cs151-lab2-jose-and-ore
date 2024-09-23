875309a0630055db11ee08d442e74b16ad9866cf
Ms_Gigi <ogadebusoye@loyola.edu>
2024-09-23 00:49:59 -0400

    Modified

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # Course:  CS151, Dr. Zelalem Jembre Yalew
 # Due Date: 9/25/2024
 # Lab Assignment: 1
-# What program does:This program that will determine the expected population
-# in the future for a country based on a current population amount given (1) how often someone is born (in seconds),
+# What program does: This program determines the expected population in the future for a country
+# based on a current population amount given (1) how often someone is born (in seconds),
 # (2) how often someone dies (in seconds), and (3) how often a new immigrant joins the country (in seconds).
 
 # Constants
@@ -34,9 +34,9 @@ future_population = int(future_population)
 if future_population > current_population:
     population_change = "increased"
 else:
-    population_change= "decreased"
+    population_change = "decreased"
 
 # Print the results
-print(f"The population in {years} years will be {future_population}")
-print(f"The population {population_change}")
+print("The population in" ,years, "years will be" ,future_population)
+print("The population" ,population_change)
 
